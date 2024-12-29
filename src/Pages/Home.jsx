f9388dacae7fb4c70a1778196c61081ea74f3302
Yardan <139103935+qwerkcar@users.noreply.github.com>
2024-12-29 17:03:25 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -80,9 +80,9 @@ const PAUSE_DURATION = 2000;
 const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
-  { icon: Github, link: "https://github.com/EkiZR" },
-  { icon: Linkedin, link: "https://www.linkedin.com/in/ekizr/" },
-  { icon: Instagram, link: "https://www.instagram.com/ekizr_/?hl=id" }
+  { icon: Github, link: "https://github.com/yardanshaq" },
+  { icon: X, link: "https://www.x.com/yardanshaq" },
+  { icon: Instagram, link: "https://www.instagram.com/yardanshaq" }
 ];
 
 const Home = () => {
