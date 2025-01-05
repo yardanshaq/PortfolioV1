a2684bd520d778aac3c55285c3215950fdf52e7b
Yardan <139103935+qwerkcar@users.noreply.github.com>
2025-01-05 12:21:51 +0700

    Update WelcomeScreen.jsx

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -152,7 +152,7 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
                 data-aos-delay="1200"
               >
                 <a
-                  href="https://yardansh.site"
+                  href="https://www.ekizr.my.id"
                   className="inline-flex items-center gap-2 px-4 py-2 sm:px-6 sm:py-3 rounded-full relative group hover:scale-105 transition-transform duration-300"
                   target="_blank"
                   rel="noopener noreferrer"
