ac790cf802e39d1f6d898b5f2cf908c30b248255
Yardan <139103935+qwerkcar@users.noreply.github.com>
2025-01-05 13:19:33 +0700

    Update WelcomeScreen.jsx

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -161,7 +161,7 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
                   <div className="relative flex items-center gap-2 text-lg sm:text-xl md:text-2xl">
                     <Globe className="w-4 h-4 sm:w-5 sm:h-5 text-indigo-600" />
                     <span className="bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
-                      <TypewriterEffect text="www.yardansh.site" />
+                      <TypewriterEffect text="yardansh.site" />
                     </span>
                   </div>
                 </a>
