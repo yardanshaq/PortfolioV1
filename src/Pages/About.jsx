a6bf6321c88e4c080b273769bdd40ff8308fbdcd
Yardan <139103935+qwerkcar@users.noreply.github.com>
2025-01-05 13:34:57 +0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -221,7 +221,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/drive/folders/1BOm51Grsabb3zj6Xk27K-iRwI1zITcpo" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/drive/u/0/folders/1lBWnJnepANDYjEzpXEKA8WM4f_qHzwSe" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
