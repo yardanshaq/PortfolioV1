5969c37f18ee65a4902dba25ef3d4ffda257cd88
Yardan <139103935+qwerkcar@users.noreply.github.com>
2025-01-05 12:18:38 +0700

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,8 +12,8 @@ const socialLinks = [
     name: "LinkedIn",
     displayName: "Let's Connect",
     subText: "on LinkedIn",
-    icon: Linkedin,
-    url: "https://www.linkedin.com/in/ekizr/",
+    icon: X,
+    url: "https://www.x.com/yardanshaq",
     color: "#0A66C2",
     gradient: "from-[#0A66C2] to-[#0077B5]",
     isPrimary: true
@@ -21,34 +21,25 @@ const socialLinks = [
   {
     name: "Instagram",
     displayName: "Instagram",
-    subText: "@ekizr_",
+    subText: "@yardanshaq",
     icon: Instagram,
-    url: "https://www.instagram.com/ekizr_/?hl=id",
+    url: "https://www.instagram.com/yardanshaq/",
     color: "#E4405F",
     gradient: "from-[#833AB4] via-[#E4405F] to-[#FCAF45]"
   },
   {
-    name: "YouTube",
-    displayName: "Youtube",
-    subText: "@eki zulfar",
-    icon: Youtube,
-    url: "https://www.youtube.com/@eki_zulfar",
-    color: "#FF0000",
-    gradient: "from-[#FF0000] to-[#CC0000]"
-  },
-  {
     name: "GitHub",
     displayName: "Github",
-    subText: "@EkiZR",
+    subText: "@qwerkcar",
     icon: Github,
-    url: "https://github.com/EkiZR",
+    url: "https://github.com/qwerkcar",
     color: "#ffffff",
     gradient: "from-[#333] to-[#24292e]"
   },
   {
     name: "TikTok",
     displayName: "Tiktok",
-    subText: "@eki_zulfar",
+    subText: "@yardanshaq",
     icon: ({ className, ...props }) => (
      <svg width="24px" height="24px" viewBox="0 0 45 45" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <title>Tiktok</title>
@@ -61,7 +52,7 @@ const socialLinks = [
     </g>
 </svg>
     ),
-    url: "https://tiktok.com/@eki_zulfar",
+    url: "https://tiktok.com/@yardanshaq",
     color: "black",
     gradient: "from-[#000000] via-[#25F4EE] to-[#FE2C55]"
   }
