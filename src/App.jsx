a2119cbfbed72d93a1cddfad8142463a6629ebd3
Yardan <139103935+qwerkcar@users.noreply.github.com>
2025-01-05 13:14:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,9 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
             <center>
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2023{" "}
+                © 2024{" "}
                 <a href="https://flowbite.com/" className="hover:underline">
-                  EkiZR™
+                  YardanShaq™
                 </a>
                 . All Rights Reserved.
               </span>
@@ -55,7 +55,7 @@ const ProjectPageLayout = () => (
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
           © 2024{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            YardanShaq
+            YardanShaq™
           </a>
           . All Rights Reserved.
         </span>
