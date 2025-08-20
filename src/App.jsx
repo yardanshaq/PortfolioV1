0fd6bda53d35f87ecdcb7734c3b1428aeeebb98e
Yardan <139103935+yardanshaq@users.noreply.github.com>
2025-08-20 11:43:50 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
                 © 2024{" "}
                 <a href="https://flowbite.com/" className="hover:underline">
-                  YardanShaq™
+                  YardanShaq
                 </a>
                 . All Rights Reserved.
               </span>
@@ -55,7 +55,7 @@ const ProjectPageLayout = () => (
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
           © 2024{" "}
           <a href="https://flowbite.com/" className="hover:underline">
-            YardanShaq™
+            YardanShaq
           </a>
           . All Rights Reserved.
         </span>
